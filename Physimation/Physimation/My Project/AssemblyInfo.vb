73dd850c557e5fd5e1e35269b4d955e69fe5ce54
Gobledegook <edendaykeegan@gmail.com>
2015-01-19 09:37:08 -0500

    Added VB project.

---
+++ Physimation/Physimation/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Physimation")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("Toshiba")> 
+<Assembly: AssemblyProduct("Physimation")> 
+<Assembly: AssemblyCopyright("Copyright © Toshiba 2015")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("a6a5c1fb-f877-43ee-9e6e-4dd6bc8d1400")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
